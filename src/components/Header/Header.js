728fac948ada0ad0ba586b1cf5154ccaa0a26cef
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-07-18 20:52:26 +0200

    className updates
    For better targetting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,8 @@ import "./Header.css"
 const Header = (props) => {
   return (
     <div className="Header">
-      <h1>Frontend Dev CS</h1>
-      <button onClick={props.onClick} type="submit">
+      <h1 className="Header-h1">Frontend Dev CS</h1>
+      <button className="Header-button" onClick={props.onClick} type="submit">
         Go
       </button>
     </div>
